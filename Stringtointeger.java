292bb301520aa0aa9e31fdacf53ba45432d1fdea
sharankumarkandasamy <sharankumar29.2@gmail.com>
2016-09-08 11:42:36 +0530

    Add files via upload

---
+++ Stringtointeger.java
@@ -0,0 +1,13 @@
+package guvi;
+
+
+
+public class Stringtointeger 
+{
+	public static void main(String[] args) 
+	{
+		String str="123";
+		int number=Integer.parseInt(str);
+		System.out.println("The number is :"+str);
+	}
+}
